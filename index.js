7d47ebec3f0cb58d5b789246b060a11409ac4f14
Andrea <acesparza05@gmail.com>
2022-06-27 17:49:41 -0500

    commit tree 6c7292c0e58993ac41bdac371c92a2baf5edc4de

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let ride;
+  if (distance <= 400) {
+    ride = "This one is on me!";
+  } 
+  else if (distance < 2000) {
+    ride = "That will be twenty bucks.";
+  } 
+  else if (distance <= 2500) {
+    ride = "I will gladly take your thirty bucks.";
+  }
+  else if (distance > 2500) {
+    ride = "No can do.";
+  }
+  return ride;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){  
+  return (city == "NYC" ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tipType){
+  let response;
+  switch(tipType) {
+    case "generous":
+      response =  "Thank you so much.";
+      break;
+    case "not as generous":
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye.";
+      break;
+  }
+  return response;
 }
